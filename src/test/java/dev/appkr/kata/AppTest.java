ac3be2d377248b2d306028db7a659afb878b2f1b
appkr <juwonkim@me.com>
2020-11-06 12:04:47 +0900

    project created

---
+++ src/test/java/dev/appkr/kata/AppTest.java
@@ -0,0 +1,15 @@
+package dev.appkr.kata;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+  @DisplayName("JUnit Jupiter가 작동한다")
+  @Test
+  public void junitShouldWorkCorrectly() {
+    assertTrue(true);
+  }
+}
